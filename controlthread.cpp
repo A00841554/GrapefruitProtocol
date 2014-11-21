45ef139d25b350b0c48c5e75785adb25db9c0355
A00841554 <surplus.et@gmail.com>
2014-11-20 17:52:32 -0800

    minimally integrated control thread with the application     - "a" starts the control thread     - "s" stops the control thread
    finished the control thread; however, it doesn't start the transmit thread

--- controlthread.cpp
+++ controlthread.cpp
@@ -3,6 +3,10 @@
 
 DWORD WINAPI fnControl(LPVOID args)
 {
+    // parse thread parameters
+    ControlArgs* controlArgs = (ControlArgs*) args;
+    OutputDebugString("Control thread started\n");
+
     // create transmit and receive structures
     TransmitArgs transmitArgs;
     ReceiveArgs receiveArgs;
@@ -26,6 +30,20 @@ DWORD WINAPI fnControl(LPVOID args)
     // enter main control loop
     while (true)
     {
+        // exit look if we're supposed to stop
+        if (controlArgs->bRequestStop) {
+            OutputDebugString("Control thread stopping\n");
+            receiveArgs.bRequestStop = true;
+            transmitArgs.bRequestStop = true;
+
+            if (receiveArgs.bStopped /*&& transmitArgs.bStopped*/)
+            {
+                OutputDebugString("Control thread stopped\n");
+                controlArgs->bStopped = true;
+                return 0;
+            }
+        }
+
         // restart any threads that are stopped if the state of both threads are
         // not active
         if (!receiveArgs.bActive && !transmitArgs.bActive)
@@ -44,6 +62,9 @@ DWORD WINAPI fnControl(LPVOID args)
                 transmitArgs.bStopped = false;
             }
         }
+
+        // sleep so we don't burn out the core
+        Sleep(CONTROL_THREAD_SLEEP_INTERVAL);
     }
 }
 
