8a7ebb78db2c704632411a33d696addb4659a66c
Jonathan Chu <jonathan_chu1988@hotmail.com>
2014-11-27 17:14:26 -0800

    Added sendData for controlChar
    check title

--- helper.h
+++ helper.h
@@ -8,11 +8,12 @@ const int DATA_SIZE = 1018;
 const int VALIDTION_SIZE = 4; //32 bits
 const int PACKET_SIZE = HEADER_SIZE + DATA_SIZE + VALIDTION_SIZE;
 
-char* fnPacketizeData(TransmitArgs &transmit, bool bForceEOT);
-bool fnCheckDuplicate (char cPacket[], ReceiveArgs &receive);
-bool fnIsEOT( char cPacket[] );
-bool fnIsETB( char cPacket[] );
-void fnProcessData(char cPacket[]);
-void fnSendData(char cPacket[], std::ofstream& commPort);
+unsigned char* fnPacketizeData(TransmitArgs &transmit, bool bForceEOT);
+bool fnCheckDuplicate (unsigned char byPacket[], ReceiveArgs &receive);
+bool fnIsEOT( unsigned char byPacket[] );
+bool fnIsETB( unsigned char byPacket[] );
+void fnProcessData(unsigned char byPacket[]);
+void fnSendData(unsigned char byPacket[], std::ofstream& commPort);
+void fnSendData(unsigned char byControlChar, HANDLE hCommPort);
 
 #endif
